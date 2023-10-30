95660ae5740d3aa3afbd8ebf1f1b0b6794a8c847
Gongsoochan <140775642+Gongsoochan@users.noreply.github.com>
2023-10-30 18:09:20 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.py
@@ -0,0 +1,13 @@
+s = input()
+alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+         'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alpha_n = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+           0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+cnt = 1
+for i in s:
+    if i in alpha:
+        for j in range(len(alpha)):
+            if alpha[j] == i:
+                alpha_n[j] += cnt
+for n in alpha_n:
+    print(n,end=' ')
