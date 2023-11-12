295aa780398d056f22a4d0ca6b2024abfedf0f4e
Gongsoochan <140775642+Gongsoochan@users.noreply.github.com>
2023-11-12 19:44:00 +0900

    [Bronze V] Title: 시험 성적, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,11 @@
+score=int(input())
+if 60<=score<=69:
+    print('D')
+elif 70<=score<=79:
+    print('C')
+elif 80<=score<=89:
+    print('B')
+elif 90<=score<=100:
+    print('A')
+else:
+    print('F')
